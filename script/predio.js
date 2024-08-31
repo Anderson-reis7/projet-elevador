375e9fd82f4ff666caf64edda151d7d474a31a56
Anderso reis <anderson.200810@gmail.com>
2024-08-31 17:43:35 -0300

    UPDATE: setando os atributos dos andares

--- script/predio.js
+++ script/predio.js
@@ -7,17 +7,19 @@
 
     const terreo = document.createElement("div");
     terreo.classList.add("terreo");
+    terreo.setAttribute("andar", "T");
     terreo.appendChild(janela);
 
     return terreo;
   }
 
-  function criarAndar() {
+  function criarAndar(numeroAndar) {
     const porta = document.createElement("div");
     porta.classList.add("porta");
 
     const andar = document.createElement("div");
     andar.classList.add("andar");
+    andar.setAttribute("andar", numeroAndar);
     andar.appendChild(porta);
 
     return andar;
@@ -27,8 +29,8 @@
     const elementosComAndares = document.querySelectorAll("[andares]");
     elementosComAndares.forEach((colunas) => {
       const qtd = +colunas.getAttribute("andares");
-      for (let i = 0; i < qtd; i++) {
-        colunas.appendChild(criarAndar());
+      for (let i = qtd; i > 0; i--) {
+        colunas.appendChild(criarAndar(i));
       }
       colunas.appendChild(criarTerreo());
     });
